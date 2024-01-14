8fb35c93e17fd1838b1de5df7feb4ddaf3db125f
KIMSEONGMIN <qlxlbp@gmail.com>
2024-01-14 21:11:06 +0900

    test02

---
+++ src/main/kotlin/com/example/velog/infra/auditing/AuditorConfig.kt
@@ -0,0 +1,17 @@
+package com.example.velog.infra.auditing
+
+import com.example.velog.domain.user.model.CustomUser
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
+import org.springframework.security.core.context.SecurityContextHolder
+import java.util.*
+
+@Configuration
+class AuditorConfig : AuditorAware<String> {
+    override fun getCurrentAuditor(): Optional<String> {
+        val authentication = SecurityContextHolder.getContext().authentication
+        val user: CustomUser = authentication.principal as CustomUser
+
+        return Optional.of(user.name) //현재 토큰의 사용자 이름을 가져온다
+    }
+}
